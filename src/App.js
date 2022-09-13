cbfbefdbe348f147600ffcd0b77eea382f1330f0
Rayhand117 <70382112+Rayhand117@users.noreply.github.com>
2022-09-13 21:25:24 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Headernya from './Header';
+import Footer from './Footer';
+import Content from './Content';
+// import './App.css';
+
+function App() {
+  return (
+    <>
+      <Headernya />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function Footer() {
+  return (
+    <footer className='text-muted py-5 border-top'>
+        <div className="container">
+            <p className="mb-1">This is My First React app in Bootstrap</p>
+            <p className="mb-0">New to React <a href='https://reactjs.org'>Visit the homepage</a></p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer;
